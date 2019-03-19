0232b166eb95576ca2c1a3514a12918baa590720
TianYio <1711839211@qq.com>
2019-03-19 20:39:25 +0800

    搭建路由模块

---
+++ src/router.js
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import React, { Component } from "react";
+
+export default class componentName extends Component {
+  render() {
+    return <div />;
+  }
+}
